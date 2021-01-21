aea1f8631c52e6668fff2d0bcc472e3f6deb4505
hanspavelson <44016837+hanspavelson@users.noreply.github.com>
2021-01-21 17:51:23 +0200

    Create indexrahaloos.php

---
+++ indexrahaloos.php
@@ -0,0 +1,54 @@
+<?php get_header() ?>
+
+<main class="content">
+	<section class="homepage">
+		    <div class="container first">
+                <div class="card bg-dark text-white">
+                    <img src="" class="card-img" alt="">
+                        <div class="card-img-overlay">
+                                <h5 class="card-title">loos</h5>
+                                    <p class="card-text"></p>
+                                        <p class="card-text"></p>
+                                        </div>
+                                </div>
+                        </div>
+
+                        <div class="container">
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <img src="http://koopia.indiead.ee/wp-content/uploads/2021/01/Group-4.png" width="420px" height="auto" alt="">
+                                        </div>
+                                    <div class="col-sm">
+                                        <img src="http://koopia.indiead.ee/wp-content/uploads/2021/01/Group-8.png" width="420px" height="auto" alt="">
+                                        </div>
+                                    <div class="col-sm">
+                                    <img src="http://koopia.indiead.ee/wp-content/uploads/2021/01/Group-6.png" width="420px" height="auto" alt="">
+                                    </div>
+                                         </div>
+                                                </div>
+
+                                                <div class="container first">
+                                            <div class="card bg-dark text-white">
+                                                 <img src="" class="card-img" alt="">
+                                                    <div class="card-img-overlay">
+                                                     <h5 class="card-title">loos</h5>
+                                                         <p class="card-text"></p>
+                                                             <p class="card-text"></p>
+                                                            </div>
+                                                            </div>
+                                                         </div>
+                                                        <div class="container last">
+                                                         <div class="row last">
+                                                            <div class="col-8">col-8</div>
+                                                             <div class="col-4" style="width:40%;">col-4</div>
+
+                                                        </div>
+                                                        </div>
+
+
+	                                                 </section>
+                                                        </main>
+
+
+
+                                                            <?php get_footer() ?>
