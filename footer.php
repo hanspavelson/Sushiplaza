218bc5adaaf856146e7e7caa3e18ff5a77551e33
hanspavelson <44016837+hanspavelson@users.noreply.github.com>
2021-01-21 17:47:52 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,88 @@
+<?php wp_footer() ?>
+</body>
+</html>
+
+<footer class="bg-light text-center text-lg-start">
+
+  <div class="container p-4">
+  <img src="http://koopia.indiead.ee/wp-content/uploads/2021/01/V.png" width="200px" height="40px" alt="" style="display:flex; margin-bottom: 20px;" >
+
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+        <h5 class="text-uppercase">Rahaloos OÜ</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+          <h6 class="text-uppercase">Narva mnt. 86</h6>
+          </li>
+          <li>
+          <h6 class="text-uppercase">Tallinn, 15120</h6>
+          </li>
+          <li>
+          <h6 class="text-uppercase">Eesti Vabariik</h6>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+
+        <ul class="list-unstyled">
+          <li>
+            <h6 href="#!" class="text-dark">Avaleht</h6>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Loosimised</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Blogi</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Kontakt</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!" class="text-dark">Üldreeglid</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Osalemistingimused</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Isikuandmete kaitse</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Vastustundlik mängimine</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!" class="text-dark">Lõppenud loosimised</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Võitude vormistamine</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">KKK</a>
+          </li>
+        </ul>
+      </div>
+      <div class="footer-img">
+<img src="http://koopia.indiead.ee/wp-content/uploads/2021/01/Group-20.png" width="181px" height="191px" alt="">
+ </div>
+    </div>
+
+
+  </div>
+
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+    <a class="text-dark">Tähelepanu! Tegemist on hasartmängu reklaamiga. Hasartmäng pole sobiv viis rahaliste probleemide lahendamiseks. Tutvuge reeglitega ja käituge vastutustundlikult!</a>
+  </div>
+
+</footer>
+<!-- Footer -->
