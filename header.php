673b2e6820ca02e35c556070a3994971a3d9fb36
hanspavelson <44016837+hanspavelson@users.noreply.github.com>
2021-01-21 17:48:15 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="mdea">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="style" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<?php wp_head(); ?>
+</head>
+<header>
+<div id="wrapper-navbar" itemscope="" itemtype="http://schema.org/WebSite">
+                <nav class="navbar navbar-expand-lg">
+  <a class="navbar-brand" href="http://koopia.indiead.ee/wp-content/uploads/2021/01/Screenshot-2021-01-20-at-14.30.16.png"width="0" height="0">
+  <img src="http://koopia.indiead.ee/wp-content/uploads/2021/01/V.png" width="200px" height="40px" alt="">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Avaleht <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Loosimised <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Blogi <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Kontakt <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item active">
+      <button class="btn my-2 my-sm-0" type="submit">Logi sisse</button>
+      </li>
+      <li class="nav-item active">
+      <button class="btn my-2 my-sm-0" type="submit">Ostukorv</button>
+      </li>
+    </ul>
+
+
+  </div>
+</nav>
+</div>
+    </header>
